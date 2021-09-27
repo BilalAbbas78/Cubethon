e493e7d949403646d4e24278b433b6da209a0978
BilalAbbas78 <abbasb000@gmail.com>
2021-09-27 19:48:16 +0500

    Initial Commit

---
+++ Cubethon - Syed Bilal Abbas/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 Offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + Offset;
+    }
+}
---
+++ Cubethon - Syed Bilal Abbas/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool isGameEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (isGameEnded == false)
+        {
+            isGameEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
